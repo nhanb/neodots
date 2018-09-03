49ba56e4b547bf37ac10e8f6b722fdc0869ef5c0
Bui Thanh Nhan <nhan@nerdyweekly.com>
2018-09-03 16:47:50 +0700

    disable cd-able vars
    Because it causes `cd <tab>` to suggest every possible envar which is
    useless and annoying.

--- bash/sensible.bash
+++ bash/sensible.bash
@@ -108,13 +108,3 @@ shopt -s cdspell 2> /dev/null
 # Add the directories you want to have fast access to, separated by colon
 # Ex: CDPATH=".:~:~/projects" will look for targets in the current working directory, in home and in the ~/projects folder
 CDPATH="."
-
-# This allows you to bookmark your favorite places across the file system
-# Define a variable containing a path and you will be able to cd into it regardless of the directory you're in
-shopt -s cdable_vars
-
-# Examples:
-# export dotfiles="$HOME/dotfiles"
-# export projects="$HOME/projects"
-# export documents="$HOME/Documents"
-# export dropbox="$HOME/Dropbox"
