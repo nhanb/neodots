0c4ace22fc7db5c0b79a35b082af495278c4f1c5
Bui Thanh Nhan <nhan@nerdyweekly.com>
2018-09-04 13:28:58 +0700

    colorful 256-color prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -5,14 +5,8 @@ export GIT_PS1_SHOWUPSTREAM="auto"
 export GIT_PS1_HIDE_IF_PWD_IGNORED=true
 export GIT_PS1_SHOWCOLORHINTS=true
 
-# Black        0;30     Dark Gray     1;30
-# Red          0;31     Light Red     1;31
-# Green        0;32     Light Green   1;32
-# Brown/Orange 0;33     Yellow        1;33
-# Blue         0;34     Light Blue    1;34
-# Purple       0;35     Light Purple  1;35
-# Cyan         0;36     Light Cyan    1;36
-# Light Gray   0;37     White         1;37
+# 256 colors:
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
 
 virtualenv_prompt () {
     if [[ $VIRTUAL_ENV != "" ]]; then
@@ -21,4 +15,4 @@ virtualenv_prompt () {
     fi
 }
 
-export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\] \[\033[01;32m\]\w\[\033[00m\]$(__git_ps1 " (%s)") $(virtualenv_prompt)\n▶ '
+export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[1;38;5;220m\]\u@\h\[\033[00m\] \[\033[1;38;5;10m\]\w\[\033[00m\]\[\033[1;38;5;159m\] $(__git_ps1 " %s")\[\033[00m\] \[\033[0;38;5;222m\]$(virtualenv_prompt)\[\033[0m\]\n▶ '
