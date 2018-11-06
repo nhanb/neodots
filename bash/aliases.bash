e6b6ea5623850ffdbf2708028fe4af62ecc0962a
Bui Thanh Nhan <nhan@nerdyweekly.com>
2018-11-06 09:24:57 +0700

    add formattest command

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -41,3 +41,10 @@ function colortest () {
         printf "\n";
     }'
 }
+
+function formattest () {
+    echo -e "\e[1mbold\e[0m"
+    echo -e "\e[3mitalic\e[0m"
+    echo -e "\e[4munderline\e[0m"
+    echo -e "\e[9mstrikethrough\e[0m"
+}
