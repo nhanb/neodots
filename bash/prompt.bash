04fcb6ef537bae349734289df9e970026a352fbc
Bui Thanh Nhan <nhan@nerdyweekly.com>
2019-01-14 18:13:56 +0700

    show exit code in prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -21,4 +21,27 @@ virtualenv_prompt () {
     fi
 }
 
-export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[1;38;5;220m\]\u@\h\[\033[00m\] \[\033[1;38;5;10m\]\w\[\033[00m\]\[\033[1;38;5;159m\] $(__git_ps1 " %s")\[\033[00m\] \[\033[0;38;5;222m\]$(virtualenv_prompt)\[\033[0m\]\n[\j]▶ '
+PROMPT_COMMAND=__prompt_command # Func to gen PS1 after CMDs
+
+__prompt_command() {
+    local EXIT="$?"             # This needs to be first
+    PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[1;38;5;220m\]\u@\h\[\033[00m\] \[\033[1;38;5;10m\]\w\[\033[00m\]\[\033[1;38;5;159m\] $(__git_ps1 " %s")\[\033[00m\] \[\033[0;38;5;222m\]$(virtualenv_prompt)\[\033[0m\]\n'
+
+    local RCol='\[\e[0m\]'
+
+    local Red='\[\e[0;31m\]'
+    local Gre='\[\e[0;32m\]'
+    local BYel='\[\e[1;33m\]'
+    local BBlu='\[\e[1;34m\]'
+    local Pur='\[\e[0;35m\]'
+
+    if [ $EXIT != 0 ]; then
+        PS1+="[${Red}${EXIT}${RCol}]"      # Add red if exit code non 0
+    else
+        PS1+="[0]"
+    fi
+    PS1+="▶ "
+    #PS1+="${RCol}@${BBlu}\h ${Pur}\W${BYel}$ ${RCol}"
+}
+
+#export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[1;38;5;220m\]\u@\h\[\033[00m\] \[\033[1;38;5;10m\]\w\[\033[00m\]\[\033[1;38;5;159m\] $(__git_ps1 " %s")\[\033[00m\] \[\033[0;38;5;222m\]$(virtualenv_prompt)\[\033[0m\]\n[\j]▶ '
