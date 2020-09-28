d87a338052428d6b1aadcaa1e1b3c618d1699c8d
Bùi Thành Nhân <hi@imnhan.com>
2020-09-28 18:13:13 +0700

    dumb launcher

--- surface-go/.config/fish/conf.d/sway.fish
+++ surface-go/.config/fish/conf.d/sway.fish
@@ -2,5 +2,7 @@
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   set -x MOZ_ENABLE_WAYLAND 1
+  set -x QT_QPA_PLATFORM wayland
+  set -x SDL_VIDEODRIVER wayland
   exec sway
 end
