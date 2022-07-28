5b98e4d7f944cf46ed1b863bdd5826facd36fe28
Nhân <hi@imnhan.com>
2022-07-28 21:39:07 +0700

    add firefox user.js

---
+++ firefox/user.js
@@ -0,0 +1,14 @@
+// Symlink this to
+// ~/.mozilla/firefox/XXXXXXXX.your_profile_name/user.js
+
+// Windows-style scrollbar, which is wider than those dumb
+// "modern" qt/gtk abominations.
+user_pref("widget.non-native-theme.scrollbar.style", 4);
+
+// Force animated loading tab icon
+user_pref("ui.prefersReducedMotion", 0);
+
+// Force light mode aka prevent smartass websites from looking like garbage
+user_pref("layout.css.prefers-color-scheme.content-override", 1);
+
+user_pref("general.smoothScroll", false);
