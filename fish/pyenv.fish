25816a0091c73e816ce660d423aebb755c7e5053
Nhân <hi@imnhan.com>
2022-12-31 14:12:35 +0700

    clean things up to use OS-provided pyenv and venv
    I'm now using `pyenv -m venv` directly instead of the pyenv-virtualenv
    plugin. Therefore, updated pymk & pyact accordingly.

--- fish/pyenv.fish
+++ fish/pyenv.fish
@@ -1,13 +1,10 @@
-set -Ux VIRTUAL_ENV_DISABLE_PROMPT 1
-set -Ux PYENV_VIRTUALENV_DISABLE_PROMPT 1
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x PYENV_VIRTUALENV_DISABLE_PROMPT 1
 
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-pyenv init - | source
-pyenv init --path | source
+set VENVS_DIR "$HOME/.venvs"
 
 # Activate virtualenv with the same name as current dir
-abbr -a -g pyact 'pyenv activate (basename $PWD)'
+abbr -a -g pyact '. $VENVS_DIR/(basename $PWD)/bin/activate.fish'
 abbr -a -g pydeact pyenv deactivate
 
 # Make pyenv virtualenv using current directory as name.
@@ -17,17 +14,19 @@ function pymk
         echo 'Must provide only 1 argument for python version'
         return 1
     end
+
     set py_version $argv
+    set venv_name (basename "$PWD")
+    set venv_dir "$VENVS_DIR/$venv_name"
 
     # Install python version if it's not already there:
     pyenv versions | cut -d / -f 1 | uniq | grep "$py_version\$"
     if [ $status != 0 ]
-        pyenv install "$py_version"
+        pyenv install "$py_version" || return 1
     else
         echo "Python version $py_version already installed."
     end
 
     echo "Creating virtualenv $venv_name"
-    set venv_name (basename "$PWD")
-    pyenv virtualenv "$py_version" "$venv_name"
+    "$HOME/.pyenv/versions/$py_version/bin/python" -m venv "$venv_dir"
 end
