60502c796de9fa5a932d0b3490fe3df5405d8183
Nhân <hi@imnhan.com>
2023-01-19 16:26:51 +0700

    fish: update git prompt configs
    As noted in
    https://fishshell.com/docs/current/relnotes.html#fish-3-6-0-released-january-7-2023
    
    > The fish_git_prompt will now only turn on features if the appropriate
    > variable has been set to a true value (of “1”, “yes” or “true”)
    > instead of just checking if it is defined.

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -5,12 +5,12 @@
 set fish_prompt_pwd_dir_length 0
 
 # show git status - keep it vanilla though
-set __fish_git_prompt_show_informative_status
-set __fish_git_prompt_showcolorhints
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_showcolorhints 1
 set __fish_git_prompt_showupstream "auto"
-set __fish_git_prompt_showdirtystate
-set __fish_git_prompt_showuntrackedfiles
-set __fish_git_prompt_showstashstate
+set __fish_git_prompt_showdirtystate 1
+set __fish_git_prompt_showuntrackedfiles 1
+set __fish_git_prompt_showstashstate 1
 
 set __fish_git_prompt_char_cleanstate "ok"
 set __fish_git_prompt_char_dirtystate "+"
