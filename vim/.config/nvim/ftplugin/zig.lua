f4f005b8d5aebe5815767d76b5f16a962c08d1c2
Nhân <hi@imnhan.com>
2025-12-06 11:13:05 +0700

    vim: <c-cr> to also insert semicolon

--- vim/.config/nvim/ftplugin/zig.lua
+++ vim/.config/nvim/ftplugin/zig.lua
@@ -2,4 +2,7 @@ vim.cmd [[
     setlocal tw=80
     :iab istd const std = @import("std");
     :iab ipri const print = std.debug.print;
+
+    " <c-cr> to insert ; at end of line and then enter
+    imap <c-cr> <cr><esc>jA;<esc>kcc
 ]]
